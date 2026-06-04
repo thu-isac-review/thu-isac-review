ce4a012cf3e3414e608cece4dc38c02915db4a02
thu-isac-review <thu.isac.review@gmail.com>
2026-06-04 14:28:39 +0800

    Update main.js

--- public/assets/js/pages/intern_view/institution/main.js
+++ public/assets/js/pages/intern_view/institution/main.js
@@ -1,4 +1,4 @@
-import { render as renderInstitutionTable } from '../../components/InstitutionTable/main.js';
+import { render as renderInstitutionTable } from '../../../components/InstitutionTable/main.js';
 
 export async function render(containerId, context) {
     // 開啟「前台瀏覽模式」：isReadOnly 為 true，隱藏所有編輯按鈕與 Checkbox
