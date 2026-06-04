54de0d390e166b46dfa9f2130f37e15ac81e99c7
thu-isac-review <thu.isac.review@gmail.com>
2026-06-04 15:40:16 +0800

    Update state.js

--- public/assets/js/components/CourseTable/state.js
+++ public/assets/js/components/CourseTable/state.js
@@ -1,1 +1,7 @@
-.
+// 管理課程表格的全域狀態
+export const state = {
+    courses: [],          // 課程列表資料
+    currentEditId: null,  // 當前正在編輯的課程 ID
+    isSubmitting: false,  // 防止重複提交表單的狀態
+    searchQuery: ''       // 搜尋關鍵字
+};
